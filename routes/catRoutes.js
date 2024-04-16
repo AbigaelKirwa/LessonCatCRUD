42c48c5e21ee7965f03b9ed1b161c31b1919c7d0
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-16 19:23:16 +0300

    feat(delete):use delete route and controller

--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -5,5 +5,6 @@ const catController = require('../controllers/catController')
 router.post('/', catController.create)
 router.get('/', catController.read)
 router.put('/:id', catController.update)
+router.delete('/:id', catController.delete)
 
 module.exports = router
