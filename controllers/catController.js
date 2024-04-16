d4cb819d4040ff0db20e55b6c956bfb4f0f4c093
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-16 19:16:09 +0300

    feat(create):use post route and controller

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,5 +1,10 @@
 var cats = ["Paddington", "Garfield"]
 
+exports.create = (req, res)=>{
+    const {name} = req.body
+    cats.push(name)
+    res.send(`added cat:${name}`);
+}
 
 exports.read = (req, res)=>{
     res.send(`cat name: ${cats}`)
